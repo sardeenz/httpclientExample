2ebc84bf612caf6079380237eec09ebf9f77f5cd
Chad Foley <chad.foley@raleighnc.gov>
2018-04-08 21:38:39 -0400

    added comments

--- src/app/my-github-user.service.ts
+++ src/app/my-github-user.service.ts
@@ -30,36 +30,41 @@ export class MyGithubUserService {
     this.fullUrl = this.baseUrl + githubUsername;
     console.log('this.url = ', this.fullUrl);
 
-    // do a 'get' on the url and return it as type GithubUserModel
+    // do a 'get' on the url and returns the observable as type GithubUserModel
     return this.http.get<GithubUserModel>(this.fullUrl);
   }
 
+
+  // I put these next 2 examples here just to illustrate an example of using the rxjs (reactive javascript) operator of map
+  // there are like 150 operators in rxjs and this is just one of them. They basically help transform observables.
+  // Map transforms the items emitted by an Observable by applying a function to each item
+
+  // do a 'get' on the url and use rxjs map operator to apply a function to each item emitted by the observable
+  // only the login value in these 2 examples is returned (rxjs map operator basically calls a function on each element returned) then
+  // returns it as the login variable within the type of GithubUserModel
+  // Map transforms the items emitted by an Observable by applying a function to each item
   getGithubUserInfoUsingRXJSMap(githubUsername: string): Observable<string> {
     // concatentate the passed in variable to the end of the url i.e. https://api.github.com/users/sardeenz
     this.fullUrl = this.baseUrl + githubUsername;
     console.log('this.url = ', this.fullUrl);
+
     return this.http.get<GithubUserModel>(this.fullUrl).map((x) => {
       return x.login + ' is a doodoohead';  // this is the function that transforms the value
+      // I could optionally just return x which is the whole populated object of GithubUserModel
     });
   }
 
-  // I put this here just to illustrate an example of using the rxjs (reactive javascript) operator of map
-  // there are like 150 operators in rxjs and this is just one of them. They basically help transform observables.
   getGithubUserLoginInfoUsingRXJSMap(githubUsername: string): Observable<string> {
     // concatentate the passed in variable to the end of the url i.e. https://api.github.com/users/sardeenz
     this.fullUrl = this.baseUrl + githubUsername;
     console.log('this.url = ', this.fullUrl);
 
-    // do a 'get' on the url and use rxjs map operator to apply a function to each item emitted by the observable
-    // only the login value in this example is returned (rxjs map operator basically calls a function on each element returned) then
-    // returns it as the login variable within the type of GithubUserModel
-    // Map transform the items emitted by an Observable by applying a function to each item
     return this.http.get<GithubUserModel>(this.fullUrl).map(({ login }) => {
       return login + ' is a doodoohead2';  // this is the function that transforms the value
     });
   }
 
-  }
+}
 
 
 
